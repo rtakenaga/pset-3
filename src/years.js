ff2c1d1226ce235b2158db046d2f5cc5a4522658
KING RIO BIRD <ryomatakenaga@gmail.com>
2019-11-05 22:51:10 -0500

    second to last commit

---
+++ src/years.js
@@ -0,0 +1,19 @@
+const readlineSync = require("readline-sync");
+
+const min = 0;
+const max = Number.MAX_SAFE_INTEGER;
+
+const year = Number(readlineSync.question("\nEnter a year: "));
+
+    if (Number.isNaN(year)) {
+    console.log("\nInvalid.")
+  } else if (year < min || year > max) {
+    console.log("\nInvalid.")
+
+  } else if (year % 4 == 0) {
+    console.log("\n" + year + " is a leap year.")
+
+  } else {
+    console.log("\n" + year + " is not a leap year.")
+    
+  }
